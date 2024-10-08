9e70c142d6f69b5ef83ec0e1e4182a316e3c09b9
hogan-choi <69132745+hogan-choi@users.noreply.github.com>
2024-10-08 10:03:03 -0400

    Update activations.py
    Added Softmax activation layer.

--- Neural_Networks/activations.py
+++ Neural_Networks/activations.py
@@ -9,6 +9,61 @@ import cupy as cp
 ## Matrix operations are compiled on CPU.
 ##########################################################################################################################################################################
 
+## A class representing the Softmax activation function.
+class Softmax():
+    '''
+    A clsas that contains the Softmax activation function as well as its derivative on CPU.
+
+    Attributes:
+        None
+
+    Methods:
+        activate (self, x): Applies the Softmax activation function on input.
+        activateGrad (self, x): Takes the derivative of the Softmax function on given input.
+
+    Usage:
+        Used as an activation function on the last layer in a Neural Network.
+    '''
+    ## Initializes the Softmax activation function on CPU.
+    def __init__(self):
+        pass
+
+    ## Applies the Softmax activation function to given input.
+    def activate(self, x): 
+        '''
+        Applies Softmax function on the outputs of the last layer in Neural Networks.
+
+        Args:
+            x (np.array): The input passed through the layer.
+
+        Returns:
+            x_prime (np.array): The output after the Softmax activation function.
+        '''
+        x_prime = (np.exp(x)) / (np.sum(np.exp(x), axis = 0))
+        return x_prime
+
+    ## Applies the gradient of the Softmax activation function to given input.
+    def activateGrad(self, x):
+        '''
+        Returns the output of the gradient of the Softmax activation function.
+
+        Args:
+            x (np.array): The input passed through the layer during backpropagation.
+
+        Returns:
+            softmax_grad (np.array): The gradient of the Softmax activation function.
+        '''
+        ## Get the Softmax activation values.
+        s = self.activate(x)
+
+        ## Create the diagonal matrix and subtract Softmax value.
+        diag_arr = np.eye(x.shape[1])[None, :, :] * s[:, :, None]
+        softmax_2d = np.einsum('ij, ik -> ijk', s, s)
+        softmax_grad = diag_arr - softmax_2d
+
+        ## Return the gradient of the Softmax activation function.
+        return softmax_grad
+
 ## A class representing the Sigmoid activation function.
 class Sigmoid():
     '''
@@ -66,6 +121,61 @@ class Sigmoid():
 ## Neural Network activation functions created using CuPy.
 ## Matrix operations are compiled on GPU.
 ##########################################################################################################################################################################
+
+## A class representing the Softmax activation function compiled on GPU.
+class Softmax_GPU():
+    '''
+    A class that contains the Softmax activation function as well as its derivative on GPU.
+
+    Attributes:
+        None
+
+    Methods:
+        activate (self, x): Applies the Softmax activation function on input.
+        activateGrad (self, x): Takes the derivative of the Softmax function on given input.
+
+    Usage:
+        Used as an activation function on the last layer in a Neural Network compiled on GPU.
+    '''
+    ## Initializes the Softmax activation function on GPU.
+    def __init__(self):
+        pass
+
+    ## Applies the Softmax activation function to given input on GPU.
+    def activate(self, x): 
+        '''
+        Applies Softmax function on the outputs of the last layer in Neural Networks using GPU.
+
+        Args:
+            x (np.array): The input passed through the layer.
+
+        Returns:
+            x_prime (np.array): The output after the Softmax activation function.
+        '''
+        x_prime = (cp.exp(x)) / (cp.sum(cp.exp(x), axis = 0))
+        return x_prime
+
+    ## Applies the gradient of the Softmax activation function to given input on GPU.
+    def activateGrad(self, x):
+        '''
+        Returns the output of the gradient of the Softmax activation function using GPU.
+
+        Args:
+            x (np.array): The input passed through the layer during backpropagation.
+
+        Returns:
+            softmax_grad (np.array): The gradient of the Softmax activation function.
+        '''
+        ## Get the Softmax activation values.
+        s = self.activate(x)
+
+        ## Create the diagonal matrix and subtract Softmax value.
+        diag_arr = cp.eye(x.shape[1])[None, :, :] * s[:, :, None]
+        softmax_2d = cp.einsum('ij, ik -> ijk', s, s)
+        softmax_grad = diag_arr - softmax_2d
+
+        ## Return the gradient of the Softmax activation function.
+        return softmax_grad
 
 ## A class representing the Sigmoid activation function compiled on GPU.
 class Sigmoid_GPU():
